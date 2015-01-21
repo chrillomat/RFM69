8dc8c94f96b250065b0e096995f841bbaa6c0923
chrillomat <chrilli@chrillomat.net>
2015-01-21 20:56:55 +0100

    Example how to switch radio controlled outlets

--- RFM69.py
+++ RFM69.py
@@ -43,14 +43,10 @@ class RFM69():
     self.CONFIG = {
       0x01: [REG_OPMODE, RF_OPMODE_SEQUENCER_ON | RF_OPMODE_LISTEN_OFF | RF_OPMODE_STANDBY],
       #no shaping
-      0x02: [REG_DATAMODUL, RF_DATAMODUL_DATAMODE_CONTINUOUSNOBSYNC | RF_DATAMODUL_MODULATIONTYPE_OOK | RF_DATAMODUL_MODULATIONSHAPING_00],
-      #0x02: [REG_DATAMODUL, RF_DATAMODUL_DATAMODE_CONTINUOUS | RF_DATAMODUL_MODULATIONTYPE_OOK | RF_DATAMODUL_MODULATIONSHAPING_00],
+      0x02: [REG_DATAMODUL, RF_DATAMODUL_DATAMODE_PACKET | RF_DATAMODUL_MODULATIONTYPE_OOK | RF_DATAMODUL_MODULATIONSHAPING_00],
       #default:4.8 KBPS
-      #0x03: [REG_BITRATEMSB, RF_BITRATEMSB_55555],
-      #0x04: [REG_BITRATELSB, RF_BITRATELSB_55555],
-      #default:5khz, (FDEV + BitRate/2 <= 500Khz)
-      #0x05: [REG_FDEVMSB, RF_FDEVMSB_50000],
-      #0x06: [REG_FDEVLSB, RF_FDEVLSB_50000],
+      0x03: [REG_BITRATEMSB, 22],
+      0x04: [REG_BITRATELSB, 208],
 
       0x07: [REG_FRFMSB, frfMSB[freqBand]],
       0x08: [REG_FRFMID, frfMID[freqBand]],
@@ -61,41 +57,22 @@ class RFM69():
       # +13dBm formula: Pout=-18+OutputPower (with PA0 or PA1**)
       # +17dBm formula: Pout=-14+OutputPower (with PA1 and PA2)**
       # +20dBm formula: Pout=-11+OutputPower (with PA1 and PA2)** and high power PA settings (section 3.3.7 in datasheet)
-      #0x11: [REG_PALEVEL, RF_PALEVEL_PA0_ON | RF_PALEVEL_PA1_OFF | RF_PALEVEL_PA2_OFF | RF_PALEVEL_OUTPUTPOWER_11111],
+      0x11: [REG_PALEVEL, RF_PALEVEL_PA0_ON | RF_PALEVEL_PA1_OFF | RF_PALEVEL_PA2_OFF | RF_PALEVEL_OUTPUTPOWER_11111],
       #over current protection (default is 95mA)
       #0x13: [REG_OCP, RF_OCP_ON | RF_OCP_TRIM_95],
 
-      0x18: [REG_LNA, RF_LNA_ZIN_200 | RF_LNA_GAINSELECT_MAX],
-
-      # RXBW defaults are { REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_24 | RF_RXBW_EXP_5} (RxBw: 10.4khz)
-      #//(BitRate < 2 * RxBw)
-      0x19: [REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_16 | RF_RXBW_EXP_5],
-        0x1B: [REG_OOKPEAK, RF_OOKPEAK_THRESHTYPE_PEAK],
-      #for BR-19200: //* 0x19 */ { REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_24 | RF_RXBW_EXP_3 },
-      #DIO0 is the only IRQ we're using
-      0x25: [REG_DIOMAPPING1, RF_DIOMAPPING1_DIO0_10],
-      #must be set to dBm = (-Sensitivity / 2) - default is 0xE4=228 so -114dBm
-        0x29: [REG_RSSITHRESH, 180],
-      #/* 0x2d */ { REG_PREAMBLELSB, RF_PREAMBLESIZE_LSB_VALUE } // default 3 preamble bytes 0xAAAAAA
-      #0x2e: [REG_SYNCCONFIG, RF_SYNC_ON | RF_SYNC_FIFOFILL_AUTO | RF_SYNC_SIZE_2 | RF_SYNC_TOL_0],
-      #attempt to make this compatible with sync1 byte of RFM12B lib
-      #0x2f: [REG_SYNCVALUE1, 0x2D],
-      #NETWORK ID
-      #0x30: [REG_SYNCVALUE2, networkID],
-      #0x37: [REG_PACKETCONFIG1, RF_PACKET1_FORMAT_VARIABLE | RF_PACKET1_DCFREE_OFF |
-      #      RF_PACKET1_CRC_ON | RF_PACKET1_CRCAUTOCLEAR_ON | RF_PACKET1_ADRSFILTERING_OFF],
-      #in variable length mode: the max frame size, not used in TX
-      #0x38: [REG_PAYLOADLENGTH, 66],
-      #* 0x39 */ { REG_NODEADRS, nodeID }, //turned off because we're not using address filtering
+      # TX: packet sent
+      0x25: [REG_DIOMAPPING1, RF_DIOMAPPING1_DIO0_00],
+      0x2C: [REG_PREAMBLEMSB, 0],
+      0x2D: [REG_PREAMBLELSB, 0],
+      0x2E: [REG_SYNCCONFIG, 0],
+      0x37: [REG_PACKETCONFIG1, RF_PACKET1_FORMAT_VARIABLE | RF_PACKET1_DCFREE_OFF |
+      #0x37: [REG_PACKETCONFIG1, RF_PACKET1_FORMAT_FIXED | RF_PACKET1_DCFREE_OFF |
+            RF_PACKET1_CRC_OFF | RF_PACKET1_CRCAUTOCLEAR_ON | RF_PACKET1_ADRSFILTERING_OFF],
+      #0x38: [REG_PAYLOADLENGTH, 16],
+      0x38: [REG_PAYLOADLENGTH, 0],
       #TX on FIFO not empty
-      #0x3C: [REG_FIFOTHRESH, RF_FIFOTHRESH_TXSTART_FIFONOTEMPTY | RF_FIFOTHRESH_VALUE],
-      #RXRESTARTDELAY must match transmitter PA ramp-down time (bitrate dependent)
-      #0x3d: [REG_PACKETCONFIG2, RF_PACKET2_RXRESTARTDELAY_2BITS | RF_PACKET2_AUTORXRESTART_ON | RF_PACKET2_AES_OFF],
-      #for BR-19200: //* 0x3d */ { REG_PACKETCONFIG2, RF_PACKET2_RXRESTARTDELAY_NONE | RF_PACKET2_AUTORXRESTART_ON | RF_PACKET2_AES_OFF }, //RXRESTARTDELAY must match transmitter PA ramp-down time (bitrate dependent)
-      #* 0x6F */ { REG_TESTDAGC, RF_DAGC_CONTINUOUS }, // run DAGC continuously in RX mode
-      # run DAGC continuously in RX mode, recommended default for AfcLowBetaOn=0
-      #0x6F: [REG_TESTDAGC, RF_DAGC_IMPROVED_LOWBETA0],
-      #0x00: [255, 0]
+      0x3C: [REG_FIFOTHRESH, RF_FIFOTHRESH_TXSTART_FIFONOTEMPTY | RF_FIFOTHRESH_VALUE],
     }
     #initialize SPI
     self.spi = spidev.SpiDev()
@@ -223,16 +200,26 @@ class RFM69():
     if (len(buff) > RF69_MAX_DATA_LEN):
       buff = buff[0:RF69_MAX_DATA_LEN]
 
-    self.spi.xfer([REG_FIFO | 0x80, len(buff) + 3, toAddress, self.address])
+    if toAddress>=0:
+        self.spi.xfer([REG_FIFO | 0x80, len(buff) + 3, toAddress, self.address])
 
-    if sendACK:
-      self.spi.xfer([0x80])
-    elif requestACK:
-      self.spi.xfer([0x40])
+        if sendACK:
+            self.spi.xfer([0x80])
+        elif requestACK:
+            self.spi.xfer([0x40])
+        else:
+            self.spi.xfer([0x00]) # TODO
+
+            self.spi.xfer([int(ord(i)) for i in list(buff)])
     else:
-      self.spi.xfer([0x00])
-
-    self.spi.xfer([int(ord(i)) for i in list(buff)])
+        #print buff
+        #self.spi.xfer(buff)
+        for elem in buff:
+            self.spi.xfer([REG_FIFO | 0x80, elem])
+        for elem in buff:
+            self.spi.xfer([REG_FIFO | 0x80, elem])
+        for elem in buff:
+            self.spi.xfer([REG_FIFO | 0x80, elem])
 
     self.setMode(RF69_MODE_TX)
     # interuptHandler will set chip to standby once TX is finished
--- example.py
+++ example.py
@@ -5,10 +5,10 @@ from RFM69registers import *
 
 test = RFM69.RFM69(RF69_433MHZ, 0x01, 0x01, False)
 print "class initialized"
-print "reading all registers"
-results = test.readAllRegs()
-for result in results:
-    print result
+#print "reading all registers"
+#results = test.readAllRegs()
+#for result in results:
+#    print result
 print "Performing rcCalibration"
 test.rcCalibration()
 #print "setting high power"
@@ -17,19 +17,13 @@ print "Checking temperature"
 print test.readTemperature(0)
 #print "sending blah to 0x02"
 #test.send(0x02, "blah", False)
-#print "setting encryption"
-#test.encrypt("1234567891011121")
-#print "sending blah to 0x03"
-#test.send(0x03, "blah", True)
 try:
-    print "reading"
-    test.receiveBegin()
-    while not test.receiveDone():
-	sleep(1)
-	#val=test.readRSSI(True)
-        val=str(hex(test.readReg(REG_IRQFLAGS1)))
-        print val
-    print test.DATA
+    while True:
+        print "sending"
+        test.send(-1, [0x8E, 0x88, 0x8E, 0x88, 0x8E, 0x8E, 0x8E, 0x8E, 0xEE, 0x88, 0x88, 0xEE, 0x80, 0x00, 0x00, 0x00], False)
+        sleep(.1)
+        #print str(hex(test.readReg(REG_OPMODE)))
+        #print str(hex(test.readReg(REG_IRQFLAGS1)))
 except KeyboardInterrupt:
     print "user interrupted!"
 finally:
